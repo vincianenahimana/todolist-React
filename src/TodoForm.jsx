e49d857384fb124551d95a88da8a4cb6b46370e3
vincianenahimana <vnahimana@gmail.com>
2024-12-16 14:52:14 +0100

    add categories options | modify classes

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -2,13 +2,17 @@ import React from "react";
 
 export function TodoForm({ onSubmit }) {
     return (
-        <form className="d-flex" onSubmit={onSubmit}>
+        <form className="d-flex flex-column flex-sm-row mb-4" onSubmit={onSubmit}>
             <input
-                className="form-control me-2"
+                className="form-control me-2 mb-2 mb-sm-0"
                 placeholder="Type a new todo"
                 name="task"
             />
-            <button className="btn btn-primary me-2" type="submit">
+            <select className="form-select form-select-sm me-2 fs-6 text" name="category">
+                <option value="Private">Private</option>
+                <option value="Work">Work</option>
+            </select>
+            <button className="btn btn-primary" type="submit">
                 Add
             </button>
         </form>
