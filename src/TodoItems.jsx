ba75119fb792ef320c35cfed8f0127196a360d14
vincianenahimana <vnahimana@gmail.com>
2024-12-16 14:54:10 +0100

    Modify style | Add misses classes

--- src/TodoItems.jsx
+++ src/TodoItems.jsx
@@ -3,11 +3,16 @@ import React from 'react'
 export function TodoItems({ todo, onStatusChange, onDelete }) {
     return (
         <div
-            className="rounded mt-4 p-2 d-flex"
-            style={{ backgroundColor: todo.done ? "#79ff4d" : "gray" }}
+            className='rounded mt-4 p-2 d-flex justify-content-between gap-3'
+            style={{ backgroundColor: todo.done ? "white" : "gray" }}
         >
-            <div className="me-auto">{todo.content}</div>
-            <div>
+            <div className="me-auto d-flex justify-content-between w-100">
+                <span>{todo.content}</span>
+                <span className={`border-end border-4 pe-2 ${todo.category === "Work" ? "border-warning" : "border-info"}`}>
+                    {todo.category}
+                </span>
+            </div>
+            <div className='d-flex align-items-center'>
                 <i
                     className={"h5 me-2 " + (todo.done ? "bi bi-check-square" : "bi bi-square")}
                     style={{ cursor: "pointer" }}
@@ -25,3 +30,4 @@ export function TodoItems({ todo, onStatusChange, onDelete }) {
         </div>
     );
 }
+
